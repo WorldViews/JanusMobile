3f08831fdf869b3a9862b25fec1ce83a440b01cf
Jim Vaughan <jimv@mrjim.com>
2017-04-24 16:12:29 -0700

    Using watchPosition instead of getCurrentPosition

--- app/lib/GPS.js
+++ app/lib/GPS.js
@@ -6,40 +6,38 @@ export default class Gps {
       options = options || {};
       this.url = options.url || "https://worldviews.org/";
       this.interval = options.interval || 1000;
-      this.timerHandle = null;
+      this.watchHandle = null;
       this.username = options.username || "Unknown";
   };
 
   start() {
       this.sock = io.connect(this.url);
 
-      this.timerHandle = setInterval(() => {
-           console.log("GPS: timer func");
-           if (navigator.geolocation) {
-               navigator.geolocation.getCurrentPosition((position) => {
-                   console.log("GPS got loc");
-                   let msg = {
-                       msgType: 'position',
-                       version: 0.1,
-                       clientType: 'android',
-                       clientId: this.username,
-                       t: new Date().getTime() / 1000.0,
-                       position: [position.coords.latitude, position.coords.longitude],
-                       coordSys: 'geo'};
-                   console.log("GPS: " + JSON.stringify(msg));
-                   this.sock.emit('position', msg);
-                   console.log("GPS emitted the msg");
-               });
-           }
-      }, this.interval);
+      if (navigator.geolocation) {
+           this.watchHandle = navigator.geolocation.watchPosition((position) => {
+               console.log("GPS got loc");
+               let msg = {
+                   msgType: 'position',
+                   version: 0.1,
+                   clientType: 'android',
+                   clientId: this.username,
+                   t: new Date().getTime() / 1000.0,
+                   position: [position.coords.latitude, position.coords.longitude],
+                   coordSys: 'geo'
+               };
+               console.log("GPS: " + JSON.stringify(msg));
+               this.sock.emit('position', msg);
+               console.log("GPS emitted the msg");
+           });
+      }
       console.log("GPS:  start");
   };
 
   stop() {
       console.log("GPS:  stop");
-      if (this.timerHandle) {
-          clearInterval(this.timerHandle);
-          this.timerHandle = null;
+      if (this.watchHandle) {
+          navigator.geolocation.clearWatch(this.watchHandler);
+          this.watchHandle = null;
       }
   };
 };
